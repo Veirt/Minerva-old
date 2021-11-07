ed52bdb31691f64744f0899f3700d04a56fa097c
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-07 17:09:26 +0800

    specify type

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,10 @@
+import { Client } from "discord.js";
 import { Event } from "../@types";
 
 const event: Event = {
     name: "ready",
     once: true,
-    execute(client) {
+    execute(client: Client<true>) {
         console.log(`Logged in as ${client.user.tag}!`);
         // set RPC (Rich Presence)
         client.user.setActivity("idk");
