5988e17ed3c849521f6aee6556398695aefa2645
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-10 23:34:32 +0800

    feat(types): add deploy env variable

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,6 +1,7 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
+            DEPLOY: boolean;
             NODE_ENV: "production" | "development";
             DISCORD_CLIENT_TOKEN: string;
             DISCORD_CLIENT_ID: string;
